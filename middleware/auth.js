476ca41720c32e89571958da4c12e5a14130d12b
igrik723 <hilkewich7@gmail.com>
2024-05-24 20:24:14 +0300

    add authMiddleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.heafers['authorization'];
+
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({error: 'Unauthorized'})
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).json({err: 'Invalid token'})
+        }
+
+        req.user = user;
+
+        next();
+    })
+}
+
+module.exports = authenticateToken;
