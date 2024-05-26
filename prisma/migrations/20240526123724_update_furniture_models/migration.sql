acba828927cfbb65c0cf12d5a1e61c4e4cd78d09
igrik723 <hilkewich7@gmail.com>
2024-05-26 15:50:22 +0300

    add role to user

---
+++ prisma/migrations/20240526123724_update_furniture_models/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `saleId` on the `FurnitureModel` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FurnitureModel" DROP COLUMN "saleId";
