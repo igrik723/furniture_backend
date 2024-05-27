a7237073c1dd364225352f02f45d956e7026aafd
igrik723 <hilkewich7@gmail.com>
2024-05-27 21:06:41 +0300

    add routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController } = require('../controllers');
+const { UserController, AgreementController, FurnitureModelsController, SaleController } = require('../controllers');
+const authenticateToken = require('../middleware/auth');
 
 const uploadDestination = 'upload'
 
@@ -14,6 +15,19 @@ const storage = multer.diskStorage({
 
 const uploads = multer({storage: storage})
 
+//Роуты пользователя
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+
+//Роуты моделей мебели
+
+router.post('/furnitureModels', authenticateToken, FurnitureModelsController.createModel)
+router.delete('/furnitureModels/:id', authenticateToken, FurnitureModelsController.createModel)
+
+//Роуты продаж
+router.post('/sale', authenticateToken, SaleController.saleModel)
+//Роуты договоров
+router.post('/agreements', authenticateToken, AgreementController.createAgreement)
+
+
 module.exports = router
